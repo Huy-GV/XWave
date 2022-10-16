1ecc55d31ab58933fb38bc9a012a1c3af850ad4a
basbaybee <emperorhuyv@gmail.com>
2022-10-16 14:32:39 +1100

    simplified queries in AuthorizationService

--- XWave.Core/Services/Implementations/AuthorizationService.cs
+++ XWave.Core/Services/Implementations/AuthorizationService.cs
@@ -1,49 +1,61 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using XWave.Core.Data;
 using XWave.Core.Models;
 using XWave.Core.Services.Interfaces;
 
 namespace XWave.Core.Services.Implementations;
 
-internal class AuthorizationService : IAuthorizationService
+internal class AuthorizationService : ServiceBase, IAuthorizationService
 {
     private readonly UserManager<ApplicationUser> _userManager;
-
-    public AuthorizationService(UserManager<ApplicationUser> userManager)
+    public AuthorizationService(
+        UserManager<ApplicationUser> userManager,
+        XWaveDbContext dbContext) : base(dbContext)
     {
         _userManager = userManager;
     }
 
     public async Task<string[]> GetRolesByUserId(string userId)
     {
-        var user = await _userManager.FindByIdAsync(userId);
-        if (user is null)
-        {
-            return Array.Empty<string>();
-        }
-
-        return (await _userManager.GetRolesAsync(user)).ToArray();
+        return await GetRolesByUserIdQuery(userId).ToArrayAsync();
     }
 
     public async Task<string[]> GetRolesByUserName(string userName)
     {
-        var user = await _userManager.FindByNameAsync(userName);
-        if (user is null)
-        {
-            return Array.Empty<string>();
-        }
+        var query = 
+            from user in DbContext.Users
+            join userRole in DbContext.UserRoles on user.Id equals userRole.UserId
+            join role in DbContext.Roles on userRole.RoleId equals role.Id
+            where user.UserName == userName
+            select role.Name;  
 
-        return (await _userManager.GetRolesAsync(user)).ToArray();
+        return await query.ToArrayAsync();
     }
 
     public async Task<bool> IsUserInRole(string userId, string role)
     {
-        return (await GetRolesByUserId(userId)).Contains(role);
+        return await GetRolesByUserIdQuery(userId).AnyAsync(x => x == role);
     }
 
     public async Task<bool> IsUserInRoles(string userId, IEnumerable<string> roles)
     {
-        return (await GetRolesByUserId(userId))
-            .Intersect(roles)
-            .Any();
+        return await GetRolesByUserIdQuery(userId).AnyAsync(x => roles.Contains(x));
+    }
+
+    private IQueryable<string> GetRolesByUserIdQuery(string userId)
+    {
+        return  DbContext.Roles
+            .Join(DbContext.UserRoles,
+                roles => roles.Id,
+                userRoles => userRoles.RoleId,
+                (role, userRole) => new 
+                { 
+                    RoleName = role.Name, 
+                    UserId = userRole.UserId 
+                })
+            .Where(x => x.UserId == userId)
+            .Select(x => x.RoleName);
     }
 }
